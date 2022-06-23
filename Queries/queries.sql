9017bc43aa7f5acb7cd8e6cc8601f614b56b9497
Gopinath Vasanthakumar <gopiv@macheye.com>
2022-06-22 21:56:38 -0400

    queries.sql checkin

---
+++ Queries/queries.sql
@@ -0,0 +1,28 @@
+SELECT * from DEPARTMENTS
+SELECT * from EMPLOYEES
+SELECT * from SALARIES
+SELECT * from DEPT_MANAGER
+
+SELECT * from TITLES
+SELECT * from DEPT_EMP
+
+DROP TABLE titles CASCADE;
+DROP TABLE dept_emp CASCADE;
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
