9ad4bd170cf99242b707167bb2408bae6a7a4880
Gopinath Vasanthakumar <gopiv@macheye.com>
2022-06-23 18:50:50 -0400

    Deliverable 2 checkin

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,5 @@
+-- Deliverable 1: The Number of Retiring Employees by Title
+
 SELECT 
 	e.emp_no, 
 	e.first_name, 
@@ -9,7 +11,7 @@ INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as t
 ON e.emp_no = t.emp_no
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY e.emp_no
 
 SELECT DISTINCT ON 
@@ -27,3 +29,22 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY emp_no DESC
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON 
+	(e.emp_no) e.emp_no,
+	e.first_name, 
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no
